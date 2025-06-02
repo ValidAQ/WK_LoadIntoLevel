415d36580aa9db0116af93ad35e06e6cda1916e5
validaq <validaq@gmail.com>
2025-06-02 13:49:32 +0200

    Update 0.1.0: * Add the option to select the scene to load into * Rename project to LoadIntoLevel to reflect the added functionality

--- Plugin.cs
+++ Plugin.cs
@@ -3,17 +3,18 @@ using BepInEx.Logging;
 using BepInEx.Configuration;
 using UnityEngine.SceneManagement;
 
-namespace WK_LoadIntoPlayground;
+namespace WK_LoadIntoLevel;
 
 
 [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
-public class LoadIntoPlayground : BaseUnityPlugin
+public class LoadIntoLevel : BaseUnityPlugin
 {
-    private const string pluginGuid = "com.validaq.loadintoplayground";
-    private const string pluginName = "WK_LoadIntoPlayground";
-    public const string pluginVersion = "0.0.1";
+    private const string pluginGuid = "com.validaq.loadintolevel";
+    private const string pluginName = "WK_LoadIntoLevel";
+    public const string pluginVersion = "0.1.0";
 
     private ConfigEntry<string> sceneToReplace;
+    private ConfigEntry<string> sceneToLoad;
     internal static new ManualLogSource Logger;
 
     private void Awake()
@@ -22,14 +23,22 @@ public class LoadIntoPlayground : BaseUnityPlugin
 
         sceneToReplace = Config.Bind(
             "General",      // The section under which the option is shown
-            "PlaygroundScene",  // The key of the configuration option in the configuration file
+            "SceneToReplace",  // The key of the configuration option in the configuration file
             "Intro", // The default value
-            "Game scene to be replaced with the Playground scene. " +
-            "Use 'Intro' to instantly load into the playground upon game launch, " +
-            "or 'Training-Level' to load into the Playground when starting the tutorial. " +
+            "Game scene to be replaced. " +
+            "Use 'Intro' to instantly load into the given scene upon game launch, " +
+            "or 'Training-Level' to load into it when starting the tutorial. " +
             "It's advised to avoid using 'Main-Menu', as it will make you "+
-            "unable to return to main menu from the playground, leaving Alt-F4 as the only way to exit."
+            "unable to return to main menu from the scene, leaving Alt-F4 as the only way to exit."
             // Description of the option to show in the config file
+        );
+
+        sceneToLoad = Config.Bind(
+            "General",
+            "SceneToLoad",
+            "Playground",
+            "The name of the scene to load instead of the specified scene. " +
+            "This should be the name of the scene you want to load into, such as 'Playground'."
         );
 
         SceneManager.sceneLoaded += OnSceneLoad;
@@ -39,10 +48,10 @@ public class LoadIntoPlayground : BaseUnityPlugin
 
     private void OnSceneLoad(Scene scene, LoadSceneMode mode)
     {
-        bool menuLoaded = scene.name == sceneToReplace.Value;
-        if (menuLoaded)
+        bool targetSceneLoaded = scene.name == sceneToReplace.Value;
+        if (targetSceneLoaded)
         {
-            SceneManager.LoadScene("Playground");
+            SceneManager.LoadScene(sceneToLoad.Value);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,10 +1,23 @@
-# White Knuckle - Load Into Playground
+# White Knuckle - Load Into Level
 
-This mod allows you to load into the Dev Playground directly upon starting the game or instead of a specific gamemode - e.g. the tutorial.
+This mod allows you to load into the specified scene directly upon starting the game or instead of a specific gamemode - e.g. the tutorial.
 
 By default, it drops you directly into the playground when you launch the game.
 
-You can choose the scene to be replaced by the playground in the mod config file.
-For example, to launch the playground instead of the tutorial, set `PlaygroundScene` to `Training-Level`.
+You can choose the scene to be replaced and the scene that will replace it in the mod config file.
+For example, to launch the playground instead of the tutorial, set `SceneToReplace` to `Training-Level` while leaving `SceneToLoad` set to `Playground`.
 
-The config file can be found at `<Game Directory>\BepInEx\config\com.validaq.loadintoplayground.cfg`
+List of available scenes:
+* `Game-Main` - the main game scene, used for all standard gameplay
+* `Game-Main-Holiday` - the special holiday event mode, aka "The Chimney"; currently doesn't seem to load properly
+* `Intro` - the intro cutscene
+* `Main-Menu` - the main menu
+* `Playground` - the dev playground level
+* `Training-Level` - the tutorial level
+
+> [!NOTE]
+> The `Game-Main` scene has additional level-loading logic tied to it, depending on what game mode was selected.
+> Currently this mod doesn't support making use of it - it will simply load the specified scene without any additional logic (WIP)
+> Refer to `M_Gamemode` and `CL_GameManager` classes in game's code for more information on the scene-loading behavior.
+
+The config file can be found at `<Game Directory>\BepInEx\config\com.validaq.loadintolevel.cfg`
